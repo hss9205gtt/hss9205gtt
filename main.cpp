5c96e4c30960bc5e19b85c96f3a7d7f140adf5ea
hss9205gtt <103564447+hss9205gtt@users.noreply.github.com>
2022-04-19 12:15:28 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,89 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char buf[100000];
+int main(){
 
-int breakAll = 0;
-char* name;
-int L = 0;
-int R = 0;
+	int a,b;
+	scanf("%d %d", &a, &b);
+	int min = a < b ? a : b;
+	int max = a < b ? b : a;
 
-vector<string> split(string str, char Delimiter) {
-    istringstream iss(str);             // istringstream에 str을 담는다.
-    string buffer;                      // 구분자를 기준으로 절삭된 문자열이 담겨지는 버퍼
+	deque<int> minMeasure;
+	deque<int> maxMeasure;
 
-    vector<string> result;
+    for(int i = 1; i <= max; i++){
+        if(max % i == 0){
 
-    // istringstream은 istream을 상속받으므로 getline을 사용할 수 있다.
-    while (getline(iss, buffer, Delimiter)) {
-        result.push_back(buffer);               // 절삭된 문자열을 vector에 저장
+            maxMeasure.push_back(i);
+        }
     }
 
-    return result;
-}
-
-int main()
-{
-    string mystring;
-    getline(cin, mystring);
-    cout<<mystring<<endl;
-
-
-    cin.getline(buf, 100000);
-
-    for(int i2 = 0, i3_start = 0, i3_end = 0; i2 < 100000; i2++)
-    {
-        str = buf;
-    }
-
-
-    printf("%s\n", str);
-
-    for(int i2 = 0, i3_start = 0, i3_end = 0; i2 < 100000; i2++)
-    {
-
-        if(buf[i2] == '<')
-        {
-            L = 1;
-            printf("%c", buf[i2]);
-            i3_start = i2 + 1;
-            continue;
-        }
-
-        if(buf[i2] == '>')
-        {
-            L = 2;
-            printf("%c", buf[i2]);
-            i3_start = i2 +1;
-        }
-
-        if(buf[i2] == ' ' || buf[i2] == '\0' || L == 1)
-        {
-            i3_end = i2;
-            for(int i3 = i3_end; i3 >= i3_start; i3--)
-            {
-                printf("%c", buf[i3]);
+    for(int i = 2, i2Start = 0; i <= min; i++){
+        if(min % i == 0){
+            for(int i2 = i2Start; i2 < maxMeasure.size(); i2++){
+            if(maxMeasure.ar(i2) == i)
+                i2Start = i2;
             }
-            i3_start = i2 + 1;
-
         }
-        else if(L == 1)
-            printf("%c", buf[i2]);
-
-
-
-
-        if(buf[i2] == '\0')
-            break;
 
 
     }
 
+    maxsize = minMeasure.size() > maxMeasure.size() ? minMeasure.size() : maxMeasure.size();
+    //최대공약수
+
+    //최소공배수
+        for(int i = 0; i < maxsize;i++){
+
+        }
 
 
 
-
-    return 0;
-
+	return 0;
 }
-
